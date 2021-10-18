a38d2594a8ec8c827636afa605f8ff090452a9bb
lce83 <90365985+l2chae@users.noreply.github.com>
2021-10-18 13:39:06 +0900

    Create 3052.py

---
+++ BOJ/3052.py
@@ -0,0 +1,11 @@
+#머리로는 이해가 되지만 구현이 조금 어려웠다.
+a = []
+for i in range(10):
+    a.append(int(input())%42)
+a_set = set(a)
+print(len(a_set))
+
+
+#더 간단하게 작성할 수 있는 방안을 찾아본 것
+a = [int(input())%10 for i in range(10)]
+print(len(set(a)))
