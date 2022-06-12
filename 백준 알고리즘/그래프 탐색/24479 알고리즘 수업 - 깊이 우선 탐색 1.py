08e42a4b5aff511fe0555edfac102ce92399921d
lce83 <90365985+l2chae@users.noreply.github.com>
2022-06-12 11:30:57 +0900

    first commit

---
+++ 백준 알고리즘/그래프 탐색/24479 알고리즘 수업 - 깊이 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def dfs(s):
+    global cnt
+    visited[s] = True
+    res[s] = cnt
+    cnt += 1
+    for k in arr[s]:
+        if not visited[k]:
+            dfs(k)
+
+n, m, r = map(int, input().split())
+arr = [[] for _ in range(n+1)]
+for i in range(m):
+    u, v = map(int, input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+
+for i in range(n):
+    arr[i+1].sort()
+
+visited = [False] * (n+1)
+res = [0] * (n+1)
+cnt = 1
+
+dfs(r)
+
+for i in range(1, n+1):
+    print(res[i])
