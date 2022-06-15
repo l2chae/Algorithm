0ca1d6317e6538c6dcf567c02d3cc149b03573f6
lce83 <90365985+l2chae@users.noreply.github.com>
2022-06-15 16:28:35 +0900

    first commit

---
+++ 백준 알고리즘/그래프 탐색/24445 알고리즘 수업 - 너비 우선 탐색 2.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def bfs(s):
+    global cnt
+    queue = deque([s])
+    visited[s] = True
+    while queue:
+        v = queue.popleft()
+        res[v] = cnt
+        cnt += 1
+        for i in arr[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+n, m, r = map(int, input().split())
+arr = [[] for _ in range(n+1)]
+for i in range(m):
+    u, v = map(int, input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+
+for i in range(n):
+    arr[i+1].sort(reverse=True)
+
+visited = [False] * (n+1)
+res = [0] * (n+1)
+cnt = 1
+
+bfs(r)
+
+for i in range(1, n+1):
+    print(res[i])
