b26780ee5ae2662e98681dfba13d738249120ec1
lce83 <90365985+l2chae@users.noreply.github.com>
2022-06-18 16:55:08 +0900

    first commit

---
+++ 백준 알고리즘/다이나믹 프로그래밍/11048 이동하기.py
@@ -0,0 +1,18 @@
+# n이 1일때 고려
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0 for j in range(m)] for i in range(n)]
+dp[0][0] = arr[0][0]
+
+for j in range(1, m):
+    dp[0][j] = dp[0][j-1] + arr[0][j]
+
+for i in range(1, n):
+    dp[i][0] = dp[i-1][0] + arr[i][0]
+
+for i in range(1, n):
+    for j in range(1, m):
+        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + arr[i][j]
+
+print(dp[n-1][m-1])
