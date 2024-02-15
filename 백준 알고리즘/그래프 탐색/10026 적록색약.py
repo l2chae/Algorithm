e02a774a5e981b712c46aba87a455a1fe5ce34f5
lce83 <90365985+l2chae@users.noreply.github.com>
2024-02-15 14:24:52 +0900

    third commit

--- 백준 알고리즘/그래프 탐색/10026 적록색약.py
+++ 백준 알고리즘/그래프 탐색/10026 적록색약.py
@@ -1,4 +1,38 @@
 # dfs 풀이
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+
+def dfs(x, y, l, v):
+    v[x][y] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and not v[nx][ny] and l[x][y] == l[nx][ny]:
+            dfs(nx, ny, l, v)
+
+n = int(sys.stdin.readline())
+l = [list(sys.stdin.readline().strip()) for _ in range(n)]
+l2 = [["R" if j == "G" else j for j in i] for i in l]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+visited1 = [[False] * n for _ in range(n)]
+visited2 = [[False] * n for _ in range(n)]
+
+res1 = 0
+res2 = 0
+for i in range(n):
+    for j in range(n):
+        if not visited1[i][j]:
+            dfs(i, j, l, visited1)
+            res1 += 1
+        if not visited2[i][j]:
+            dfs(i, j, l2, visited2)
+            res2 += 1
+
+print(res1, res2)
 
 # bfs 풀이
 import sys
