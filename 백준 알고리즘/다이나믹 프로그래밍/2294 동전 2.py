903a188904e87ed0ce5bc20f1bacd95c184cefa9
lce83 <90365985+l2chae@users.noreply.github.com>
2024-03-19 13:44:20 +0900

    first commit

---
+++ 백준 알고리즘/다이나믹 프로그래밍/2294 동전 2.py
@@ -0,0 +1,16 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+coins = [int(sys.stdin.readline().strip()) for _ in range(n)]
+
+dp = [100001] * (k+1)
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] = min(dp[i], dp[i-coin] + 1)
+
+if dp[-1] == 100001:
+    print(-1)
+else:
+    print(dp[-1])
